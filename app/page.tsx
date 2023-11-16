7c885f49fadd2de31503bbfff3c22e59f5795b1a
Milorad <milorad.smilj@jsguru.io>
2023-11-16 10:30:04 +0100

    api call fix

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import TicketCard from './(components)/TicketCard';
 
 const getTickets = async () => {
   try {
-    const res = await fetch('http://localhost:3000/api/Tickets', {
+    const res = await fetch('https://ticketing-tawny.vercel.app/api/Tickets', {
       cache: 'no-cache',
       method: 'GET',
       headers: {
